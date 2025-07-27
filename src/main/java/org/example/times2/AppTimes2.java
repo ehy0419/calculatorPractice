89946616e1d02c8812ea45d7aba3e595072da456
ehy0419 <2hy0419@naver.com>
2025-07-28 01:29:08 +0900

    feat. result 연산값 가져오기

--- src/main/java/org/example/times2/AppTimes2.java
+++ src/main/java/org/example/times2/AppTimes2.java
@@ -10,14 +10,14 @@ public class AppTimes2 {
 
         /// 양의 정수 입력 받기
         while(true) {
-            System.out.print("첫 번째 숫자를 입력하세요.");
+            System.out.print("첫 번째 숫자를 입력하세요: ");
             int firstNumber = sc2.nextInt();
             if (firstNumber < 0) {
                 System.out.println("음수는 입력할 수 없습니다. 프로그램을 재시작합니다.");
                 continue;
             }
 
-            System.out.print("두 번재 숫자를 입력하세요.");
+            System.out.print("두 번재 숫자를 입력하세요: ");
             int secondNumber = sc2.nextInt();
             if (secondNumber < 0) {
                 System.out.println("음수는 입력할 수 없습니다. 프로그램을 재시작합니다.");
@@ -28,7 +28,13 @@ public class AppTimes2 {
             System.out.print("사칙연산 기호를 입력하세요 (+, -, *, /): ");
             char operator = sc2.next().charAt(0);
 
-            double result = cal2
+            double result = cal2.calculateMachine(firstNumber, secondNumber, operator);
+            // cal2 스캐너 객체.
+            // calcultaeMachine Calcultor2 클래스의 사칙연산 메서드
+
+            System.out.println("결과 : " + result);
+            // result Calcultor2 클래스 연산값 가져오기
+
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String answer = sc2.next();
--- src/main/java/org/example/times2/Calculator2.java
+++ src/main/java/org/example/times2/Calculator2.java
@@ -2,26 +2,30 @@ package org.example.times2;
 
 public class Calculator2 {
 
-    public double calculate(int firstNumber, int secondNumber, char operator) {
+    public double calculateMachine(int firstNumber, int secondNumber, char operator) {
         double result;
 
         // 계산기 사칙연산
         switch (operator) {
             case '+':
-                System.out.println("결과: " + (firstNumber + secondNumber));
+                result = firstNumber + secondNumber;
+//                System.out.println("결과: " + (firstNumber + secondNumber));
                 break;
             case '-':
-                System.out.println("결과: " + (firstNumber - secondNumber));
+                result = firstNumber - secondNumber;
+//                System.out.println("결과: " + (firstNumber - secondNumber));
                 // () 처리 안 하니 연산자 - 계산 안됨
                 break;
             case '*':
-                System.out.println("결과: " + (firstNumber * secondNumber));
+                result = firstNumber * secondNumber;
+//                System.out.println("결과: " + (firstNumber * secondNumber));
                 break;
             case '/':
                 if (secondNumber == 0) {
                     throw new IllegalArgumentException("나눗셈 연산에서 분모(두 번째 정수)가 0일 수 없습니다.");
                 }
-                System.out.println("결과: " + (firstNumber / secondNumber));
+                result = firstNumber * secondNumber;
+//                System.out.println("결과: " + (firstNumber / secondNumber));
                 break;
             default:
                 throw new IllegalArgumentException("나눗셈 연산에서 분모(두 번째 정수)가 0일 수 없습니다.");
