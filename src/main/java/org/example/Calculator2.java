2f1f1e463dd32a88ca4eb21753540985b28fafa4
ehy0419 <2hy0419@naver.com>
2025-07-27 22:18:00 +0900

    계산기 작동 확인

--- src/main/java/org/example/Calculator2.java
+++ src/main/java/org/example/Calculator2.java
@@ -1,4 +1,29 @@
 package org.example;
 
 public class Calculator2 {
+
+    public double calculate(int firstNumber, int secondNumber, char operator) {
+        double result;
+        switch (operator) {
+            case '+':
+                result = firstNumber + secondNumber;
+                break;
+                case '-':
+                    result = firstNumber - secondNumber;
+                    break;
+                    case '*':
+                        result = firstNumber * secondNumber;
+                        break;
+                        case '/':
+                        result = firstNumber / secondNumber;
+                        if (secondNumber == 0) {
+                            throw new IllegalArgumentException("나눗셈 연산에서 분모(두 번째 정수)가 0일 수 없습니다.");
+                        }
+                        result = (double) firstNumber / secondNumber;
+                        break;
+                        default:
+                            throw new IllegalArgumentException("사칙연산 기호는 사칙연산 기호는 +, -, *, / 중 하나여야 합니다.");
+        }
+        return result;
+    }
 }
---
+++ src/main/java/org/example/times1/Times1.java
@@ -0,0 +1,48 @@
+package org.example.times1;
+
+import org.example.Calculator2;
+
+import java.util.Scanner;
+
+public class Times1 {
+    public static void main(String[] args) {
+        // new 이후 클래스 가져오기
+        Scanner sc = new Scanner(System.in);
+
+        // Calculator2 클래스 생성
+        Calculator2 calculator = new Calculator2();
+
+        // while (true) { - 반복문 내용 - } 반복문
+        while (true) {
+            // sout
+            System.out.println("첫 번째 숫자를 입력하세요: ");
+            // 정수 숫자는 int 로
+            int firstNumber = sc.nextInt();
+            // firstNumber의 출처는 Calculator2 에서 매개변수 선언!
+            if (firstNumber < 0) {
+                System.out.println("음수는 입력할 수 없습니다. 프로그램을 재시작합니다.");
+                continue;
+            }
+            System.out.println("두 번째 숫자를 입력하세요: ");
+            int secondNumber = sc.nextInt();
+            // secondNumber의 출처는 Calculator2 에서 매개변수 선언!
+            if (secondNumber < 0) {
+                System.out.println("음수는 입력할 수 없습니다. 프로그램을 재시작합니다.");
+                continue;
+            }
+
+            System.out.println("사칙연산 기호를 입력하세요 (+, -, *, /): ");
+            char operator = sc.next().charAt(0);
+
+            double result = calculator.calculate(firstNumber, secondNumber, operator);
+
+            System.out.println("결과: " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            if ("exit".equals(sc.next())) {
+                break;
+            }
+
+        }
+    }
+}
