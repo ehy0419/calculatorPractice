12b76e6c9dbccd94ca2007bb84dbc249ad61cc46
ehy0419 <2hy0419@naver.com>
2025-07-28 01:12:38 +0900

    feat. 계산기 1단계 과제부터 해보기

--- src/main/java/org/example/times2/AppTimes2.java
+++ src/main/java/org/example/times2/AppTimes2.java
@@ -7,24 +7,69 @@ public class AppTimes2 {
         Scanner sc2 = new Scanner(System.in);
 
         /// 양의 정수 입력 받기
-        while(true){
-            System.out.println("첫 번째 숫자를 입력하세요.");
+        while(true) {
+            System.out.print("첫 번째 숫자를 입력하세요.");
             int firstNumber = sc2.nextInt();
-            if(firstNumber < 0){
+            if (firstNumber < 0) {
                 System.out.println("음수는 입력할 수 없습니다. 프로그램을 재시작합니다.");
                 continue;
             }
 
-            System.out.println("두 번재 숫자를 입력하세요.");
+            System.out.print("두 번재 숫자를 입력하세요.");
             int secondNumber = sc2.nextInt();
-            if(secondNumber < 0){
+            if (secondNumber < 0) {
                 System.out.println("음수는 입력할 수 없습니다. 프로그램을 재시작합니다.");
                 continue;
             }
 
+            ///  사칙연산 기호 입력받기
+            System.out.println("사칙연산 기호를 입력하세요 (+, -, *, /): ");
+            char operator = sc2.next().charAt(0);
+
+            switch (operator) {
+                case '+':
+                    System.out.println(firstNumber + secondNumber);
+                    break;
+                case '-':
+                    System.out.println(firstNumber - secondNumber);
+                    break;
+                case '*':
+                    System.out.println(firstNumber * secondNumber);
+                    break;
+                case '/':
+                    if (secondNumber == 0) {
+                        throw new IllegalArgumentException("나눗셈 연산에서 분모(두 번째 정수)가 0일 수 없습니다.");
+                    }
+                    System.out.println(firstNumber / secondNumber);
+                    break;
+                default:
+                    throw new IllegalArgumentException("나눗셈 연산에서 분모(두 번째 정수)가 0일 수 없습니다.");
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String answer = sc2.next();
+            if ("exit".equals(answer)) {
+                break;
+            }
 
 
 
+            ///  차이점 확인하기
+//            if ("exit".equals(answer)) {
+//                break;
+//            }
+
+//            if ("exit".equalsIgnoreCase(answer)) {
+//                break;
+//            }
+
+//            if (answer.equals("exit")) {
+//                break;
+//            }
+
+//            if (answer.equalsIgnoreCase("exit")) {
+//                break;
+//            }
         }
     }
 }
